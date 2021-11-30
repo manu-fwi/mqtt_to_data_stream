a96a1cd5f7a8a78c03ee3bf03a6e2a9a23b2c8ed
manu <eallaud@gmail.com>
2021-11-29 23:05:00 -0400

    Use DB_FILE from config now

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,6 @@ else:
     config.config.load_config("config.txt")
 
     #Start DB engine
-engine = create_engine('sqlite:///app.db', echo=True)
+engine = create_engine(config.config.get_config("DB_FILE"), echo=True)
 models.Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine,expire_on_commit=False)
